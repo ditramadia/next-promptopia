9f22ff37682436ac680d6021106887d55c65093f
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-07-30 19:53:54 +0700

    add navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,26 @@
-import '@/styles/globals.css';
+import "@/styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-            <main className="app">
-                {children}
-            </main>
-        </body>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
   );
 };
